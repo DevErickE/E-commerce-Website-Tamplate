4c04389889bc9bdad54d9b719b007dfe5183a0a9
DevErickE <erickmarcos.ferreira003@gmail.com>
2025-02-19 07:22:52 -0300

    Update - Correção

--- index.js
+++ index.js
@@ -13,12 +13,9 @@ function validateEmail() {
 
     // Verificar se o email corresponde ao padrão
     if (!emailPattern.test(email) || email === "") {
-        errorMessage.style.color = "red"; // Mensagem vermelha de erro
         emailInput.style.border = "2px solid red"; // Borda vermelha para indicar erro
         alert("Por favor, insira um email válido!");
     } else {
-        errorMessage.textContent = "Email válido!";
-        errorMessage.style.color = "green"; // Mensagem verde de sucesso
         emailInput.style.border = "2px solid green"; // Borda verde para indicar sucesso
         alert("Você se inscreveu com sucesso!");
     }
